52eda622bf65acb214f821d52e8aca632c7fd332
h06-Cpy <hyjun0103@gmail.com>
2023-06-04 12:03:55 +0900

    made early stopping in scraping

--- preproc.py
+++ preproc.py
@@ -1,5 +1,5 @@
 import re
-from nltk.tokenize import TweetTokenizer
+from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 import nltk
@@ -8,85 +8,117 @@ import string
 nltk.download('stopwords')
 nltk.download('wordnet')
 
-tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True, reduce_len=True)
 lemmatizer = WordNetLemmatizer()
 stop_words = set(stopwords.words('english'))
 stop_words = stop_words | {'would', 'im', 'ive', 'thing', 'thats', 'probably', 'someone', 'think', 'actually', 'cant',
                            'much', 'anything', 'something', 'lol', 'stuff', 'bit', 'really', 'still', 'shit',
                            'literally', 'even', 'always', 'time', 'never', 'little', 'yeah', 'didnt', 'pretty',
                            'friend', 'nothing', 'maybe', 'look', 'lot', 'came', 'isnt', 'theyre', 'dont', 'doesnt',
-                           'um', 'ye', 'hmm', 'un', 'uh', 'eh', 'huh', 'ya', 'yo', 'yea', 'ah', 'nah', 'fuck',
-                           'fucking'}
+                           'um', 'ye', 'hmm', 'un', 'uh', 'eh', 'huh', 'ya', 'yo', 'yea', 'ah', 'nah', 'fuck', 'u',
+                           'uu', 'youre', 'oh', 'fucking', 'apple', 'amazon', 'microsoft', 'google', 'nvidia', 'tesla'}
 
-def preprocess(text:str, comp_name: str):
 
-    stop_words.add(comp_name)
+def preprocess(text: str):
+    """리트윗, 특수문자, 숫자, #, @유저명, 구두점, 불용어 지우기 + 토큰화, 표제어추출 전부"""
 
     # remove unnecessary words
     text = re.sub(r'^RT[\s]+', '', text)
-    text = re.sub(r'(\\u[0-9A-Fa-f]+)',r'', text)
-    text = re.sub(r'[^\x00-\x7f]',r'',text)
-    text = re.sub('((www\.[^\s]+)|(https?://[^\s]+))','',text)
+    text = re.sub(r'^rt[\s]+', '', text)
+    text = re.sub(r'(\\u[0-9A-Fa-f]+)', r'', text)
+    text = re.sub(r'[^\x00-\x7f]', r'', text)
+    text = re.sub('((www\.[^\s]+)|(https?://[^\s]+))', '', text)
     text = re.sub(r'[0-9]', '', text)
     text = re.sub(r'#', '', text)
-    text = re.sub('@[^\s]+','',text)
+    text = re.sub('@[^\s]+', '', text)
     text = text.translate(str.maketrans('', '', string.punctuation))
+    text = removeEmoticons(text)
+
+    text = text.lower()
 
     # tokenizing and lemmatization
-    tweet_tokens = tokenizer.tokenize(text)
+    tweet_tokens = word_tokenize(text)
     tweet_tokens = [t for t in tweet_tokens if t not in stop_words]
-    if len(tweet_tokens)<4:
+    if len(tweet_tokens) < 4:
         return None
     lemm_tokens = [lemmatizer.lemmatize(token) for token in tweet_tokens]
     return ' '.join(lemm_tokens)
 
 
-def removeUnicode(text):
-    """ Removes unicode strings like "\u002c" and "x96" """
-    text = re.sub(r'(\\u[0-9A-Fa-f]+)',r'', text)
-    text = re.sub(r'[^\x00-\x7f]',r'',text)
+def remove_waste(text):
+    text = re.sub(r'^RT[\s]+', '', text)
+    text = re.sub(r'^rt[\s]+', '', text)
+    text = re.sub(r'(\\u[0-9A-Fa-f]+)', r'', text)
+    text = re.sub(r'[^\x00-\x7f]', r'', text)
+    text = re.sub('((www\.[^\s]+)|(https?://[^\s]+))', '', text)
+    text = re.sub(r'[0-9]', '', text)
+    text = re.sub(r'#', '', text)
+    text = re.sub('@[^\s]+', '', text)
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    text = removeEmoticons(text)
     return text
+
+
+def removeUnicode(text):
+    """ 특수문자 지우기 """
+    text = re.sub(r'(\\u[0-9A-Fa-f]+)', r'', text)
+    text = re.sub(r'[^\x00-\x7f]', r'', text)
+    return text
+
 
 def replaceURL(text):
-    """ Replaces url address with "url" """
-    text = re.sub('((www\.[^\s]+)|(https?://[^\s]+))','url',text)
-    text = re.sub(r'#([^\s]+)', r'\1', text)
+    """ url 지우기 """
+    text = re.sub('((www\.[^\s]+)|(https?://[^\s]+))', '', text)
     return text
 
-def replaceAtUser(text):
-    """ Replaces "@user" with "atUser" """
-    text = re.sub('@[^\s]+','atUser',text)
+
+def removeAtUser(text):
+    """ @유저명 지우기 """
+    text = re.sub('@[^\s]+', '', text)
     return text
+
 
 def removeHashtagInFrontOfWord(text):
-    """ Removes hastag in front of a word """
-    text = re.sub(r'#([^\s]+)', r'\1', text)
+    """  # 지우기 """
+    text = re.sub(r'#', r'', text)
     return text
 
+
 def removeNumbers(text):
-    """ Removes integers """
+    """ 숫자 지우기 """
     text = re.sub(r'[0-9]', '', text)
     return text
 
-def replaceMultiExclamationMark(text):
-    """ Replaces repetitions of exlamation marks """
-    text = re.sub(r"(\!)\1+", ' multiExclamation ', text)
-    return text
 
-def replaceMultiQuestionMark(text):
-    """ Replaces repetitions of question marks """
-    text = re.sub(r"(\?)\1+", ' multiQuestion ', text)
-    return text
+def tokenizing_lemmatization(text):
+    tweet_tokens = word_tokenize(text.lower())
+    lemm_tokens = [lemmatizer.lemmatize(token) for token in tweet_tokens]
+    return ' '.join(lemm_tokens)
 
-def replaceMultiStopMark(text):
-    """ Replaces repetitions of stop marks """
-    text = re.sub(r"(\.)\1+", ' multiStop ', text)
-    return text
+
+def remove_stopwords(text):
+    tweet_tokens = word_tokenize(text.lower())
+    tweet_tokens = [t for t in tweet_tokens if t not in stop_words]
+    return ' '.join(tweet_tokens)
+
+
+def stopwords_lemma(text):
+    tweet_tokens = word_tokenize(text.lower())
+    tweet_tokens = [t for t in tweet_tokens if t not in stop_words]
+    lemm_tokens = [lemmatizer.lemmatize(token) for token in tweet_tokens]
+    return ' '.join(lemm_tokens)
+
+
+def remove_punc(text):
+    return text.translate(str.maketrans('', '', string.punctuation))
+
 
 def removeEmoticons(text):
     """ Removes emoticons from text """
-    text = re.sub(':\)|;\)|:-\)|\(-:|:-D|=D|:P|xD|X-p|\^\^|:-*|\^\.\^|\^\-\^|\^\_\^|\,-\)|\)-:|:\'\(|:\(|:-\(|:\S|T\.T|\.\_\.|:<|:-\S|:-<|\*\-\*|:O|=O|=\-O|O\.o|XO|O\_O|:-\@|=/|:/|X\-\(|>\.<|>=\(|D:', '', text)
+    text = re.sub(
+        ':\)|;\)|:-\)|\(-:|:-D|=D|:P|xD|X-p|\^\^|:-*|\^\.\^|\^\-\^|\^\_\^|\,-\)|\)-:|:\'\(|:\(|:-\(|:\S|T\.T|\.\_\.|:<|:-\S|:-<|\*\-\*|:O|=O|=\-O|O\.o|XO|O\_O|:-\@|=/|:/|X\-\(|>\.<|>=\(|D:',
+        '', text)
     return text
+
 # print(preprocess('''In honor of
 # @MarvelStudios
 # ’ #GotGVol3 we’re giving away this never opened Microsoft Zune. We have no idea if it works. Like and RT for a chance to win! US 18+. Ends 5/17/23. Rules: https://msft.it/6002gX0tC. #ZuneSweepstakes'''))
--- scraping.py
+++ scraping.py
@@ -4,15 +4,19 @@ from models import Tweet
 from database import Session
 from datetime import datetime
 
+
 def scrape_twitter(query: str, companyName: str):
     scraper = TwitterSearchScraper(query)
     session = Session()
 
-    i=0
+    i = 0
+    tmp_i = 0
+    no_change_cnt = 0
     for tweet in scraper.get_items():
+        tmp_i = i
         hashtags = ' '.join(tweet.hashtags) if tweet.hashtags is not None else None
         t = Tweet(id=tweet.id, rawContent=tweet.rawContent, renderedContent=tweet.renderedContent, lang=tweet.lang,
-                  tweetDate=tweet.date, createdDate = datetime.now(), url=tweet.url, replyCount=tweet.replyCount,
+                  tweetDate=tweet.date, createdDate=datetime.now(), url=tweet.url, replyCount=tweet.replyCount,
                   retweetCount=tweet.replyCount, likeCount=tweet.likeCount, quoteCount=tweet.quoteCount,
                   conversationId=tweet.conversationId, source=tweet.source, sourceUrl=tweet.sourceUrl,
                   sourceLabel=tweet.sourceLabel, viewCount=tweet.viewCount, hashtags=hashtags, companyName=companyName)
@@ -20,11 +24,22 @@ def scrape_twitter(query: str, companyName: str):
             session.add(t)
             session.commit()
             print(i)
-            i+=1
+            i += 1
+            no_change_cnt = 0
         except IntegrityError:
             session.rollback()
 
-companies = ['apple', 'amazon', 'microsoft', 'google', 'intel', 'ibm', 'qualcomm', 'nvidia', 'tesla']
+        if i - tmp_i == 0:
+            no_change_cnt += 1
+
+        if no_change_cnt > 199: # 연속으로 200번 트윗 못긁어오면 중지
+            print('too many conflicts with tweets in db')
+            break
+
+
+companies = ['apple', 'amazon', 'microsoft', 'google', 'nvidia', 'tesla']
+# companies = ['tesla']
+
 for company in companies:
     if company == 'apple':
         scrape_twitter('#Apple', company)
@@ -32,4 +47,4 @@ for company in companies:
 
     else:
         scrape_twitter(f'{company}', company)
-
+    scrape_twitter(f'{company}', company)
